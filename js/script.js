86f90d509ef1fbe416224ec91adb248b8e1ffd6e
qsrdev <matteo.albertini747@gmail.com>
2025-04-14 16:23:22 +0200

    feat: task end

--- js/script.js
+++ js/script.js
@@ -6,35 +6,47 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 
-/**
- * Raccolta dati
- * - KM da percorrere
- * - Età del passeggero
- *
- * Costanti
- * - prezzo biglietto al km = 0.21 €
- *
- * Esecuzione logica
- * Chiedi all'utente quanti anni ha
- * Chiedi all'uutente quanti km deve percorrere
- * Calcolare quanto è la distanza del viaggio
- * totalPrice = distanceTotal * priceKm
- *
- * Calcolo lo sconto
- * u18discount = totalPrice * 20 / 100
- * o65discount = totalPrice * 40 / 100
- *
- * u18discountPrice = totalPrice - u18discount
- * 065discountPrice = totalPrice - 065discount
- *
- *
- * let messaggio
- *
- * SE età dell'user è <18
- * message "Lo sconto è di ${u18discountPrice}
- * els
- *
- * Output:
- * Stampa del risultato
- *
- */
+const priceKm = 0.21;
+const discount20 = 20;
+const discount40 = 40;
+
+const distanceTotal = prompt("Inserire quanti Kilomtri vanno percorsi");
+console.log("Distanza totale del viaggio" + " " + distanceTotal + " km");
+
+const userAge = prompt("Inserire quanti anni hai");
+console.log("L'età dell'utente è di" + " " + parseInt(userAge));
+
+if (isNaN(distanceTotal) || isNaN(userAge)) {
+  alert("I dati inseriti sono sbagliati");
+  setTimeout(function () {
+    location.reload();
+  }, 100);
+}
+
+const totalPrice = distanceTotal * priceKm;
+console.log("Il prezzo totale del viaggio è di" + " " + totalPrice);
+
+//Blocco logico
+console.log("====Calcolo degli sconti====");
+const u18discount = (totalPrice * discount20) / 100;
+console.log("Lo sconto se hai meno di 18 anni è di" + " " + u18discount);
+
+const o65discount = (totalPrice * discount40) / 100;
+console.log("Lo sconto se sei over 65 è di" + " " + o65discount);
+
+console.log("====Calcolo prezzo scontato====");
+const u18discountPrice = totalPrice - u18discount;
+console.log("Prezzo del viaggio se hai meno di 18 anni è di" + " " + u18discountPrice);
+
+const o65discountPrice = totalPrice - o65discount;
+console.log("Prezzo del viaggio se hai più di 65 anni è di" + " " + o65discountPrice);
+
+let messagge;
+
+if (userAge < 18) {
+  messagge = `Ha diritto allo sconto under 18 - totale costo del biglietto ${u18discountPrice.toFixed(2)} €`;
+} else if (userAge >= 65) {
+  messagge = `Ha diritto allo sconto over 65 - totale costo del biglietto ${o65discountPrice.toFixed(2)} €`;
+} else {
+  messagge = `Non ha diritto allo sconto - il prezzo è di ${totalPrice.toFixed(2)} €`;
+}
